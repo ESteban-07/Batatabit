e31c9b2a53fb94571ecf7e1ba254e869b80cc801
Esteban Portillo <esteban.07.elias@gmail.com>
2023-05-17 10:08:16 -0600

    Implement next/previous button functionality and modify Sass toggle classes

--- js/main.js
+++ js/main.js
@@ -7,3 +7,47 @@ ctaButton.addEventListener('click', function () {
     plansSection.scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+const nextButton = document.getElementById('next-arrow');
+const previosButton = document.getElementById('previous-arrow');
+const coinsTable = document.getElementById('coins-table');
+const commissionsTable = document.getElementById('commissions-table');
+
+nextButton.addEventListener('click', function () {
+  toggleElementsVisibility({
+    on: [
+      { element: commissionsTable, className: '--hide' },
+      { element: previosButton, className: '--off' },
+    ],
+    off: [
+      { element: coinsTable, className: '--hide' },
+      { element: nextButton, className: '--off' },
+    ],
+  });
+});
+
+previosButton.addEventListener('click', function () {
+  toggleElementsVisibility({
+    on: [
+      { element: coinsTable, className: '--hide' },
+      { element: nextButton, className: '--off' },
+    ],
+    off: [
+      { element: commissionsTable, className: '--hide' },
+      { element: previosButton, className: '--off' },
+    ],
+  });
+});
+
+function toggleElementsVisibility({ off, on }) {
+  const elementsToDisplay = on;
+  const elementsToHidde = off;
+
+  elementsToDisplay?.forEach(({ element, className }) => {
+    element.classList.remove(className);
+  });
+
+  elementsToHidde?.forEach(({ element, className }) => {
+    element.classList.add(className);
+  });
+}
